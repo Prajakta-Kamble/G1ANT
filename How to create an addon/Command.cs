69e8a31664578ed9f503901d8a7db80deaed09c1
Prajakta-Kamble <67152975+Prajakta-Kamble@users.noreply.github.com>
2020-06-30 18:46:12 +0530

    Add files via upload

---
+++ How to create an addon/Command.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.MyAddon4
+{
+    [Command(Name = "mycommand", Tooltip = "This is a demo command")]
+    class MyCommand : Command
+    {
+        public MyCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : CommandArguments
+        {
+            [Argument(Name = "message", Required = true, Tooltip = "Type in some text to display")]
+            public TextStructure message { get; set; } = new TextStructure();
+        }
+        public void Execute(Arguments arguments)
+        {
+            RobotMessageBox.Show(arguments.message.Value);
+        }
+    }
+    
+}
